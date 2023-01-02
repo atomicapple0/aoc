8419fb114e757cb058b5babc26ab9139bd11a191
atomicapple <brianzhangaa@gmail.com>
2023-01-02 17:27:03 -0500

    01b

--- 01a-calorie.py
+++ 01a-calorie.py
@@ -1,6 +1,6 @@
-with open('calorie') as f:
+with open('01-calorie') as f:
     raw = f.read()
 
-elf = max([sum([int(cal_str) for cal_str in cals_str.split('\n')]) for cals_str in raw.split('\n\n')])
+elf = [sum([int(cal_str) for cal_str in cals_str.split('\n')]) for cals_str in raw.split('\n\n')]
 
-print(elf)
+print(max(elf))
